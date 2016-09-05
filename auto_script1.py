2b13a56585e958c88eba8f84f74df9fbe9b16a65
Sahil Potnis <spp66@cornell.edu>
2016-09-05 14:40:04 -0700

    Commiting - log.txt | team_info.txt | auto_script.py
    ~SAHIL P ON (SEP 5 2:39 PM)

--- auto_script1.py
+++ auto_script1.py
@@ -1,2 +1,3 @@
 print "auto_script1 is auto uploaded to the server (remote host) Bitnami_EC2 from PyCharm ~SAHIL"
 print "Working Directory on Remote Host:/home/bitnami/Auto_Scripts"
+print "Checking Updates..."
